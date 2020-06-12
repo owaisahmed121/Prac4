b1a746ffe62275c1de84bbed64eb96b1566fc1a5
owaisahmed121 <66171716+owaisahmed121@users.noreply.github.com>
2020-06-13 04:19:17 +0800

    Create test_binary_search_list_tree.py

---
+++ test_binary_search_list_tree.py
@@ -0,0 +1,23 @@
+from binary_search_list_tree import BinarySearchListTree
+import unittest
+
+
+class TestBinarySearchListTree(unittest.TestCase):
+    def setUp(self) -> None:
+        self.my_tree = BinarySearchListTree()
+
+    def test_find(self) -> None:
+        # TODO: Add 3 or more unit tests
+        pass
+
+    def test_insert(self) -> None:
+        # TODO: Add 3 or more unit tests
+        pass
+
+    def test_iter(self) -> None:
+        # TODO: Add 3 or more unit tests
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
